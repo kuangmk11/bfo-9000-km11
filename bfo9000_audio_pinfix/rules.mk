553997e1821088fc1a61b4baeea29da99391686d
kuangmk11 <kuangmk11@gmail.com>
2023-10-11 16:57:25 -0700

    Add files via upload

---
+++ bfo9000_audio_pinfix/rules.mk
@@ -0,0 +1,24 @@
+# Build Options
+#   change yes to no to disable
+#
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+BOOTMAGIC_ENABLE = yes       # Enable Bootmagic Lite
+AUDIO_DRIVER = pwm_hardware
+AUDIO_ENABLE = yes           # Audio output
+COMMAND_ENABLE = yes        # Commands for debug and configuration
+CONSOLE_ENABLE = yes         # Console for debug
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+KEY_OVERRIDE_ENABLE = no
+LTO_ENABLE = yes
+MOUSEKEY_ENABLE = yes       # Mouse keys
+MIDI_ENABLE = no
+NKRO_ENABLE = no            # Enable N-Key Rollover
+RGBLIGHT_ENABLE = no       # Enable WS2812 RGB underlight.
+SERIAL_DRIVER = vendor
+SPLIT_KEYBOARD = yes
+
+
+
+
+
+
